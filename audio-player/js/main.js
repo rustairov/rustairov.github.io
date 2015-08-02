9e9e01edd9ee73438a1136bff415b9a64dd7ec90
Rustam Tairov <rustairov@gmail.com>
2015-08-03 02:03:14 +0300

    Player changes

--- audio-player/js/main.js
+++ audio-player/js/main.js
@@ -1,1 +1,62 @@
-console.log('hi!');
+window.AudioContext = window.AudioContext||window.webkitAudioContext;
+
+$(function() {
+    var player = new Player();
+    
+    $('button.btn-file :file').change(function(e) {
+        var file = e.target.files[0];
+        
+        player.load(file);
+        
+        ID3.loadTags(file.name, function() {
+            var tags = ID3.getAllTags(file.name);
+            player.artist = tags.artist;
+            player.title = tags.title;
+            
+            if (player.artist && player.title) {
+                $('#name').html(player.artist + ' - ' + player.title);
+            } else {
+                $('#name').html(player.fileName);
+            }
+            
+            if (tags.picture) {
+                var image = tags.picture;
+                var base64String = '';
+                image.data.forEach(function(n) { base64String += String.fromCharCode(n); });
+                $('.cover').css('background-image', 'url(data:image/' + image.format + ';base64,' + window.btoa(base64String) + ')');
+            } else {
+                $('.cover').css('background-image', '');
+            }
+        }, { 
+            tags: ['artist', 'title', 'album', 'year', 'genre', 'lyrics', 'picture'],
+            dataReader: FileAPIReader(file)
+        });
+    });
+    
+    $('button[aria-label="Play"]').click(function(e) {
+        console.log('Play');
+        player.play();
+    });
+    
+    $('button[aria-label="Stop"]').click(function(e) {
+        console.log('Stop');
+        player.stop();
+    });
+    
+    $('button[aria-label="+"]').click(function(e) {
+        player.volumeUp();
+        
+        if (player.volume >= 1.0 ) {
+            //disable
+        }
+    });
+    
+    $('button[aria-label="-"]').click(function(e) {
+        player.volumeDown();
+        
+        if (player.volume <= -1.0 ) {
+            //disable
+        }
+    });
+    
+});
---
+++ audio-player/js/player.js
@@ -0,0 +1,55 @@
+
+var Player = function() {
+    /* Class variables */
+    this.fileName = '';
+    this.title = '';
+    this.artist = '';
+    this.isPlaying = false;
+    this.volume = 1;
+    
+    
+};
+
+Player.prototype.load = function(file, callback) {
+    var context = new (window.AudioContext || window.webkitAudioContext)();
+    this._gain = context.createGain();
+    this._gain.connect(context.destination);
+    this._gain.gain.value = this.volume;
+    this._source = context.createBufferSource();
+    this._source.connect(this._gain);
+    this._source.connect(context.destination);
+    
+    this._reader = new FileReader();
+    this._reader.onload = function(e) {
+        context.decodeAudioData(e.target.result, function(buffer) {
+            this._source.buffer = buffer;
+        }.bind(this), function(error) {
+            console.error('Error with decoding audio data: ' + error.err);
+        });
+    }.bind(this);
+    this._reader.readAsArrayBuffer(file);
+    
+    this.fileName = file.name;
+    
+    if (callback) { callback(); }
+};
+
+Player.prototype.play = function() {
+    this._source.start(0);
+    //this._source[this._source.start ? 'start' : 'noteOn'](0);
+    this.isPlaying = true;
+};
+
+Player.prototype.stop = function() {
+    this._source.stop(0);
+    //this._source[this._source.stop ? 'stop' : 'noteOff'](0);
+    this.isPlaying = false;
+};
+
+Player.prototype.volumeUp = function() {
+    this.volume = this._gain.gain.value += .1;
+};
+
+Player.prototype.volumeDown = function() {
+    this.volume = this._gain.gain.value -= .1;
+};
