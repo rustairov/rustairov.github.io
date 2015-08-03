ee78df28b81e06757f1cfb42ef1cdf4fc8257b39
Rustam Tairov <rustairov@gmail.com>
2015-08-04 01:15:41 +0300

    -

--- audio-player/js/player.js
+++ audio-player/js/player.js
@@ -8,13 +8,26 @@ var Player = function() {
 	this.volume = 1;
 
 	var context = new (window.AudioContext || window.webkitAudioContext)();
-	this._gain = context.createGain();
+	
+    this._gain = context.createGain();
 	this._gain.connect(context.destination);
 	this._gain.gain.value = this.volume;
-	this._source = context.createBufferSource();
-	this._source.connect(this._gain);
-	this._source.connect(context.destination);
-
+	
+    this._analyser = context.createAnalyser();
+    this._analyser.connect(context.destination);
+    this._analyser.minDecibels = -140;
+    this._analyser.maxDecibels = 0;
+    //TODO what is it?
+    this._analyser.fftSize = 64;
+    //this._analyser.smoothingTimeConstant = .8;
+    this._freqs = new Uint8Array(this._analyser.frequencyBinCount);
+    
+    this._source = context.createBufferSource();
+    this._source.connect(context.destination);
+    this._source.loop = true;
+    this._source.connect(this._gain);
+    this._source.connect(this._analyser);
+    
 	this._reader = new FileReader();
 	this._reader.onload = function(e) {
 		context.decodeAudioData(e.target.result, function(buffer) {
@@ -34,8 +47,35 @@ Player.prototype.load = function(file, callback) {
     if (callback) { callback.apply(this); }
 };
 
+Player.prototype.visualize = function() {
+    
+    this._analyser.getByteFrequencyData(this._freqs);
+    
+    var canvas = document.querySelector('canvas');
+    canvas.width = document.querySelector('.cover').offsetWidth - 1;
+    canvas.height = document.querySelector('.cover').offsetHeight - 1;
+    var context = canvas.getContext('2d');
+    
+    for (var i = 0; i < this._analyser.frequencyBinCount; i++) {
+        var value = this._freqs[i];
+        var percent = value / 256;
+        var height = canvas.height * percent;
+        var offset = canvas.height - height - 1;
+        var barWidth = canvas.width / this._analyser.frequencyBinCount;
+        var hue = i / this._analyser.frequencyBinCount * 360;
+        /* diffrent styles */
+        //context.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+        context.fillStyle = 'rgba(51,122,183,.3)';
+        context.fillRect(i * barWidth, offset, barWidth, height);
+    }
+    
+    requestAnimationFrame(this.visualize.bind(this));  
+};
+
+
 Player.prototype.play = function() {
     this._source.start(0);
+    this.visualize();
     //this._source[this._source.start ? 'start' : 'noteOn'](0);
     this.isPlaying = true;
 };
